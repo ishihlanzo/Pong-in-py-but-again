c9a187adfd8e3a1c841abac2a3c6506b41bdd909
shamiiow <ishihlanzo@gmail.com>
2022-02-22 22:15:58 +0100

    beaucoup trop de changement

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from re import X
-from shutil import move
 from player_template import*
-
+import random
 
 
 import pygame
+
 
 
 FPS = 60
@@ -16,9 +15,13 @@ class game:
         self.screen = screen
         self.running = True
         self.clock = pygame.time.Clock()
-        self.player1 = player(30, 405)
-        self.player2 = player(970, 405)
-        self.square = square(100, 100)
+        self.player1 = player(100, 405-45)
+        self.player2 = player(900, 405-45)
+        self.square = square(450, 425)
+        self.direction = 1
+        self.orientation = random.uniform(0, 0.5)
+        self.speed_up = 0
+
 
     def handling_event(self):
         for event in pygame.event.get() :
@@ -39,9 +42,39 @@ class game:
         else : 
             self.player2.velocity[1] = 0
         
-        if movement == True :
-            self.square.velocity[1] = 0
-            self.square.velocity[0] = 0.25
+        if movement :
+            self.square.velocity[1] = 1 * self.orientation
+            self.square.velocity[0] = 0.5 * self.direction
+
+        if (905 >= self.square.rect.x >= 850) and (len(set(range(self.square.rect.y-25, self.square.rect.y+25)) & set(range(self.player2.rect.y, self.player2.rect.y+180))) != 0 ):
+            self.direction = -1 - self.speed_up
+            self.speed_up += 0.01
+
+        
+        if (95 <= self.square.rect.x <= 110 ) and (len(set(range(self.square.rect.y-50, self.square.rect.y+50)) & set(range(self.player1.rect.y, self.player1.rect.y+180))) != 0 )  :
+            self.direction = 1 + self.speed_up
+            self.speed_up += 0.01
+        
+        if self.square.rect.y>=850 :
+            wait = self.direction
+            self.direction = 0
+            if self.orientation < 0.40 :
+                self.orientation -= 0.025
+            else :
+                self.orientation = -0.4
+            self.direction = wait
+            
+            print(self.orientation)
+        
+        if self.square.rect.y<=0 :
+            wait = self.direction
+            self.direction = 0
+            if self.orientation > -0.40 :
+                self.orientation += 0.025
+            else :
+                self.orientation = 0.4
+            self.direction = wait
+            print(self.orientation)
         
         
 
@@ -51,10 +84,6 @@ class game:
         self.player1.move()
         self.player2.move()
         self.square.move()
-        '''
-        if self.player2.colliderect(self.player2, self.square) :
-            print('aaa')
-        '''
 
     def display(self) :
         self.screen.fill('black')
@@ -74,5 +103,6 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 900))
 game = game(screen)
 game.run()
+
 
 pygame.quit()
--- player_template.py
+++ player_template.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class player:
     def __init__(self,x ,y) :
@@ -16,6 +17,19 @@ class player:
 class square:
     def __init__(self,x ,y) :
         self.image = pygame.image.load('square.png')
+        self.rect = self.image.get_rect(x=x, y=y)
+        self.speed = 15
+        self.velocity = [0, 0]
+    
+    def move(self) :
+        self.rect.move_ip(self.velocity[0] * self.speed, self.velocity[1] * self.speed)
+
+    def draw(self, screen) :
+        screen.blit(self.image, self.rect)
+
+class test:
+    def __init__(self,x ,y) :
+        self.image = pygame.image.load('téléchargé.jfif')
         self.rect = self.image.get_rect(x=x, y=y)
         self.speed = 15
         self.velocity = [0, 0]
